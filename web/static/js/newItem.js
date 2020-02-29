bf47992e6dafd9af4ae15aa05e68bce1243bf397
Surya Jasper <suryajasper2004@gmail.com>
2020-02-29 15:04:38 -0800

    Added fda api stuff

--- web/static/js/newItem.js
+++ web/static/js/newItem.js
@@ -1,4 +1,87 @@
 var select = document.getElementById('rooms');
+var parent = document.getElementById('parent');
+parent.classList.add('ignore-css');
+
+async function getDrugInfo(drugName) {
+  var axiosToSend = 'https://api.fda.gov/drug/ndc.json?search=generic_name:"' + drugName + '"&limit=100';
+  const response = await axios.get((axiosToSend), {});
+  console.log(response);
+  processDrug(response["data"]["results"]);
+}
+
+function processDrug(drugs) {
+  console.log(drugs);
+  for (var i = 0; i < drugs.length; i++) {
+    addDrugToScreen(drugs[i]);
+  }
+}
+
+function createH4(text, param) {
+  var h4 = document.createElement('h4');
+  h4.innerHTML = text + ": " + param;
+  return h4;
+}
+
+function createList(stuff) {
+  var ul = document.createElement('ul');
+  for (var i = 0; i < stuff.length; i++) {
+    var li = document.createElement('li');
+    li.innerHTML = stuff[i];
+    ul.appendChild(li);
+  }
+  return ul;
+}
+
+function addDrugToScreen(drug) {
+  var fieldset = document.createElement('fieldset');
+  fieldset.style.width = "80%";
+
+  var legend = document.createElement('legend');
+  var legendHeader = document.createElement('h3');
+
+  legendHeader.innerHTML = drug.generic_name;
+
+  var ingredients = drug.active_ingredients;
+
+  var brandName = createH4("Brand Name", drug.brand_name);
+  var labeler = createH4("Labeler", drug.labeler_name);
+  var dosage = createH4("Type", drug.dosage_form);
+  var ingD = createH4("ingredients", "");
+
+  var listofingredients = [];
+  for (var i = 0; i < ingredients.length; i++) {
+    listofingredients.push(ingredients[i].name + " (" + ingredients[i].strength + ")");
+  }
+  var actList = createList(listofingredients);
+
+  var buttonToCreate = document.createElement('button');
+  buttonToCreate.innerHTML = "Add";
+  buttonToCreate.onclick = function() {
+    window.location = "../template/mainPage.html";
+  }
+
+  var br1 = document.createElement('br');
+  var br2 = document.createElement('br');
+  var br3 = document.createElement('br');
+  var br4 = document.createElement('br');
+  var br5 = document.createElement('br');
+
+  fieldset.appendChild(brandName);
+  fieldset.appendChild(br1);
+  fieldset.appendChild(labeler);
+  fieldset.appendChild(br2);
+  fieldset.appendChild(dosage);
+  fieldset.appendChild(br3);
+  fieldset.appendChild(ingD);
+  fieldset.appendChild(br4);
+  fieldset.appendChild(actList);
+  fieldset.appendChild(br5);
+  fieldset.appendChild(buttonToCreate);
+
+  legend.appendChild(legendHeader);
+  fieldset.appendChild(legend);
+  parent.appendChild(fieldset);
+}
 
 function createOption(roomName) {
   var option = document.createElement('option');
